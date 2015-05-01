b0dccd74a599bc3976d300ea3ecbf4f530dd1bcb
Chris LaRose <cjlarose@gmail.com>
2015-04-30 22:34:08 -0700

    cjl - Add de-jong-ifs fn

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -10,3 +10,9 @@
 
 (defn foo [a b]
   (* a b))
+
+(defn de-jong-ifs [a b c d]
+  {:pre [(every? #(and (<= % js/Math.PI) (>= % (- js/Math.PI))) [a b c d])]}
+  (fn [[x y]]
+    [(- (js/Math.sin (* a y)) (js/Math.cos (* b x)))
+     (- (js/Math.sin (* c x)) (js/Math.cos (* d y)))]))
