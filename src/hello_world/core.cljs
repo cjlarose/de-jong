5af98b3c8e3903791fd389ced325ede137ce41e7
Chris LaRose <cjlarose@gmail.com>
2015-05-02 14:38:22 -0700

    cjl - Move out image data and color functions

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -26,39 +26,45 @@
 
 (defonce my-context (do (get-context)))
 
+(defn get-index [[w h] [x y]]
+            (* 4 (+ (* w (.floor js/Math y)) (.floor js/Math x))))
+
+(defn get-color [{size :size data :data} [x y]]
+  (let [idx (get-index size [x y])]
+    [(aget data idx)
+     (aget data (+ 1 idx))
+     (aget data (+ 2 idx))
+     (aget data (+ 3 idx))]))
+
+(defn set-color [{size :size data :data} [x y] [r g b a]]
+  (let [idx (get-index size [x y])]
+    (aset data idx r)
+    (aset data (+ 1 idx) g)
+    (aset data (+ 2 idx) b)
+    (aset data (+ 3 idx) a)))
+
+(defn blend-colors [mode [rx gx bx ax] [ry gy by ay]]
+  (let [apply-mode (fn [a b] (.floor js/Math (* 255 (mode (/ a 255) (/ b 255)))))]
+    [(apply-mode rx ry) (apply-mode gx gy) (apply-mode bx by) (apply-mode ax ay)]))
+
+(defn screen [a b]
+  (- 1 (* (- 1 a) (- 1 b))))
+
 (defn render-ifs [ifs num-points]
   (let [all-points  (take num-points (iterate ifs [0 0]))
         {[w h] :size ctx :context} my-context
-        image-data (.createImageData ctx w h)
-        data (.-data image-data)
-        get-index (fn [[x y]]
-                    (* 4 (+ (* w (.floor js/Math y)) (.floor js/Math x))))
-        get-color (fn [[x y]]
-                    (let [idx (get-index [x y])]
-                      [(aget data idx)
-                       (aget data (+ 1 idx))
-                       (aget data (+ 2 idx))
-                       (aget data (+ 3 idx))]))
-        set-color (fn [[x y] [r g b a]]
-                    (let [idx (get-index [x y])]
-                      (aset data idx r)
-                      (aset data (+ 1 idx) g)
-                      (aset data (+ 2 idx) b)
-                      (aset data (+ 3 idx) a)))
-        blend-colors (fn [mode [rx gx bx ax] [ry gy by ay]]
-                       (let [apply-mode (fn [a b] (.floor js/Math (* 255 (mode (/ a 255) (/ b 255)))))]
-                           ;; (println (str "blending " [rx gx gx ax] " and " [ry gy by ay] " into " 
-                           ;; [(apply-mode rx ry) (apply-mode gx gy) (apply-mode bx by) (apply-mode ax ay)])
-                           ;; )
-                           [(apply-mode rx ry) (apply-mode gx gy) (apply-mode bx by) (apply-mode ax ay)]))
-        add-color (fn [pos color]
-                    (set-color pos (blend-colors (fn [a b] (- 1 (* (- 1 a) (- 1 b)))) (get-color pos) color)))
         fill-image-data (fn [points]
-                           (doseq [[x y] points]
-                             ;;(println (str "setting " (* (+ x 2) 200) ", " (* (+ y 2) 200)))
-                             (add-color [(* (+ x 2) 200) (* (+ y 2) 200)] [0 192 0 64])))]
-    (fill-image-data all-points)
-    (.putImageData ctx image-data 0 0)))
+          (let [image-data (.createImageData ctx w h)
+                data-with-size {:data (.-data image-data) :size [w h]}
+                add-color (fn [pos color]
+                            (set-color
+                              data-with-size
+                              pos
+                              (blend-colors screen (get-color data-with-size pos) color)))]
+            (doseq [[x y] points]
+              (add-color [(* (+ x 2) 200) (* (+ y 2) 200)] [0 192 0 64]))
+            image-data))]
+    (.putImageData ctx (fill-image-data all-points) 0 0)))
 
 ;; (def my-ifs (hello/de-jong-ifs 0.97 -1.9 1.38 -1.5))
 ;; (hello/render-ifs my-ifs 10)
