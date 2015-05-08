86e8944cf706a1993fc2e788cef38320221c4b8e
Chris LaRose <chris.larose@mycase.com>
2015-05-07 23:46:05 -0700

    cjl - Render points with three

--- src/de_jong/ifs_viewer.cljs
+++ src/de_jong/ifs_viewer.cljs
@@ -1,6 +1,7 @@
 (ns de-jong.ifs-viewer
   (:require [om.core :as om]
-            [om.dom :as dom]))
+            [om.dom :as dom]
+            [cljsjs.three]))
 
 (def points-to-draw 1e4)
 
@@ -10,25 +11,14 @@
     [(- (js/Math.sin (* a y)) (js/Math.cos (* b x)))
      (- (js/Math.sin (* c x)) (js/Math.cos (* d y)))]))
 
-(defn setup-canvas [owner]
-  (let [canvas (om/get-node owner "canvas")
-        context (.getContext canvas "2d")]
-    (set! (.-fillStyle context) "rgba(0, 192, 0, 0.5)")
-    (.scale context 200 200)
-    (.translate context 2 2)))
+(defn points-to-vertices [points]
+  (apply array (map (fn [[x y]] (js/THREE.Vector3. x y 1)) points)))
 
-(defn render-in-canvas [owner [w h] points]
-  (let [canvas  (om/get-node owner "canvas")
-        context (.getContext canvas "2d")]
-    (.clearRect context -2 -2 4 4)
-    (doseq [[x y] points]
-      (.fillRect context x y 1e-2 1e-2))))
-
-(defn set-new-points [owner]
-  (let [ifs               (om/get-state owner :ifs)
-        old-points        (om/get-state owner :points)
-        new-points        (vec (map ifs old-points))]
-    (om/set-state! owner :points new-points)))
+(defn get-new-points [owner]
+  (let [ifs        (om/get-state owner :ifs)
+        old-points (om/get-state owner :points)
+        new-points (vec (map ifs old-points))]
+    new-points))
 
 (defn random-points [minimum maximum]
   (let [difference (- maximum minimum)
@@ -40,22 +30,35 @@
     (reify
       om/IInitState
       (init-state [_]
-        {:points (take points-to-draw (random-points (- js/Math.PI) js/Math.PI))
-         :ifs (de-jong-ifs a b c d)})
+        (let [geometry (js/THREE.Geometry.)
+              scene    (js/THREE.Scene.)
+              camera   (js/THREE.PerspectiveCamera. 75 (/ w h) 0.1 1000)
+              material (js/THREE.PointCloudMaterial. #js { :size 0.02 })
+              cloud    (js/THREE.PointCloud. geometry material)]
+          (.add scene cloud)
+          (set! (.-z (.-position camera)) 6)
+          { :points (take points-to-draw (random-points (- js/Math.PI) js/Math.PI))
+            :ifs (de-jong-ifs a b c d)
+            :geometry geometry
+            :scene scene
+            :camera camera }))
       om/IDidMount
       (did-mount [_]
-        (setup-canvas owner)
-        (set-new-points owner))
+        (let [renderer (js/THREE.WebGLRenderer. #js { :canvas (om/get-node owner "canvas") })]
+          (om/update-state! owner (fn [prev] (merge prev {:points (get-new-points owner)
+                                                          :renderer renderer})))))
       om/IDidUpdate
       (did-update [_ _ _]
-        (let [points (om/get-state owner :points)]
-          (render-in-canvas owner [w h] points))
-        (set-new-points owner))
+        (let [{:keys [geometry points renderer scene camera]} (om/get-state owner)]
+          (set! (.-vertices geometry) (points-to-vertices points))
+          (set! (.-verticesNeedUpdate geometry) true)
+          (.render renderer scene camera)
+          (om/update-state! owner (fn [prev] (merge prev {:points (get-new-points owner)})))))
       om/IWillReceiveProps
       (will-receive-props [_ _]
         (let [ifs (de-jong-ifs a b c d)
               points (take points-to-draw (random-points (- js/Math.PI) js/Math.PI))]
-          (om/update-state! owner (fn [_] {:points points :ifs ifs}))))
+          (om/update-state! owner (fn [prev] (merge prev {:points points :ifs ifs})))))
       om/IRender
       (render [_]
         (dom/div #js {:id "ifs-viewer"}
