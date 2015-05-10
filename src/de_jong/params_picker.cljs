d73b248398cb5fab5adf66f89d50b31034edf1bd
Chris LaRose <cjlarose@gmail.com>
2015-05-09 21:55:42 -0700

    cjl - Basic circular sliders

--- src/de_jong/params_picker.cljs
+++ src/de_jong/params_picker.cljs
@@ -2,28 +2,38 @@
   (:require [om.core :as om]
             [om.dom :as dom]))
 
-(defn parse-value [e]
-  (js/Number.parseFloat (.. e -target -value)))
+(defn handle-click [owner on-change e]
+  (let [rect  (.getBoundingClientRect (.-target e))
+        cx    (/ (+ (.-left rect) (.-right rect)) 2)
+        cy    (/ (+ (.-top rect) (.-bottom rect)) 2)
+        x     (.-clientX e)
+        y     (.-clientY e)
+        angle (js/Math.atan2 (- y cy) (- x cx))]
+    (on-change angle)))
+
+(defn circular-slider [{:keys [value diameter on-change] :or {diameter 200}} owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div
+        #js { :style #js { :width diameter
+                           :height diameter
+                           :transform (str "rotate(" value "rad)") }
+              :onClick (partial handle-click owner on-change)
+              :className "circular-slider" } ))))
 
 (defn slider [label value on-change]
-  (dom/input #js { :type "range"
-                   :id (str "param-" label)
-                   :min -3.14
-                   :max 3.14
-                   :step "0.01"
-                   :value value
-                   :onChange (comp on-change parse-value) }))
-
+  (dom/div nil
+    (dom/p nil label)
+    (dom/p nil value)
+    (om/build circular-slider {:value value :on-change on-change})))
 
 (defn param-picker [{:keys [label value on-change]} owner]
   (reify
     om/IRender
     (render [this]
       (dom/li nil
-        (dom/label
-          #js {:htmlFor (str "param-" label)}
-          label
-          (slider label value on-change))))))
+        (slider label value on-change)))))
 
 (defn handle-change [params i v]
   (om/transact! params (fn [prev] (assoc prev i v))))
