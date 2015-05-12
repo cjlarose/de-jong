163705f9ddc2479f9b6ab7dd8bde68675776e2b0
Chris LaRose <cjlarose@gmail.com>
2015-05-11 21:12:17 -0700

    cjl - Refactor params picker

--- src/de_jong/params_picker.cljs
+++ src/de_jong/params_picker.cljs
@@ -3,30 +3,27 @@
             [om.dom :as dom]
             [de-jong.circular-slider :refer [circular-slider]]))
 
-(defn slider [label value on-change]
-  (dom/li #js { :className "param-picker" }
-    (dom/div #js { :className "param-display" }
-      (dom/span #js { :className "param-label" } label)
-      (dom/span #js { :className "param-value" } (.toFixed value 3)))
-    (om/build circular-slider {:value value :on-change on-change})))
-
 (defn param-picker [{:keys [label value on-change]} owner]
   (reify
     om/IRender
     (render [this]
-      (slider label value on-change))))
-
-(defn handle-change [params i v]
-  (om/transact! params (fn [prev] (assoc prev i v))))
+      (dom/li #js { :className "param-picker" }
+        (dom/div #js { :className "param-display" }
+          (dom/span #js { :className "param-label" } label)
+          (dom/span #js { :className "param-value" } (.toFixed value 3)))
+        (om/build circular-slider {:value value :on-change on-change})))))
 
 (defn params-picker [params owner]
   (reify
     om/IRender
     (render [this]
       (let [param-labels ["α" "β" "γ" "δ"]
-            picker-props (fn [i label v] { :label label
-                                           :value v
-                                           :on-change (partial handle-change params i) })]
+            handle-change (fn [i v]
+                            (om/transact! params (fn [prev] (assoc prev i v))))
+            picker-props (fn [i label v]
+                           { :label label
+                             :value v
+                             :on-change (partial handle-change i) })]
         (dom/section #js {:id "params-picker"}
           (apply dom/ul nil
             (om/build-all param-picker (map picker-props (range 4) param-labels params))))))))
