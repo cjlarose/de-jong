c1d3e08cc28e6628549e8d541ed9f50ec2b2ae26
Chris LaRose <cjlarose@gmail.com>
2015-05-17 16:03:13 -0700

    cjl - Add transition params fn

--- src/de_jong/transition.cljs
+++ src/de_jong/transition.cljs
@@ -26,7 +26,7 @@
   {:pre [(and (>= t 0) (<= t 1))]}
   (+ initial (* t (- final initial))))
 
-(defn lerp-vectors [initial final num-frames]
+(defn lerp-vectors [num-frames initial final]
   (let [step     (/ 1 num-frames)
         vec-lerp (fn [t i f] (map (partial lerp t) i f))]
     (map #(vec-lerp % initial final) (range 0 1 step))))
@@ -39,8 +39,19 @@
     (< x (- js/Math.PI)) (+ x tau)
     :else x))
 
-(defn lerp-vectors-torus [initial final num-frames]
+(defn lerp-vectors-torus [num-frames initial final]
   (let [displacement    (displacement-vector initial final)
         effective-final (map + initial displacement)
         normalize       (partial map wrap-around)]
-    (map normalize (lerp-vectors initial effective-final num-frames))))
+    (map normalize (lerp-vectors num-frames initial effective-final))))
+
+(defn transition-params
+  "For a sequence of de jong parameters
+  ((a_0 b_0 c_0 d_0) (a_1 b_1 c_1 d_1) (a_2 b_2 c_2 d_2) ...), and a number of
+  frames, returns another sequence of de jong paramters with num-frames frames
+  interposed
+  ((a_0 b_0 c_0 d_0) {num-frames more frames} (a_1 b_1 c_1 d_1)
+  {num-frame more frames} (a_2 b_2 c_2 d_2) ...)"
+  [num-frames xs]
+  (let [pairs (partition 2 1 xs)]
+    (mapcat (fn [[a b]] (lerp-vectors-torus num-frames a b)) pairs)))
