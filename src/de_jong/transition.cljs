a8ad3bc41189ba4dda9d3039a9216e40709255a0
Chris LaRose <cjlarose@gmail.com>
2015-05-18 21:45:34 -0700

    cjl - Transitions wrap around to the front

--- src/de_jong/transition.cljs
+++ src/de_jong/transition.cljs
@@ -12,11 +12,15 @@
 
 (defn transition-params
   "For a sequence of de jong parameters
-  ((a_0 b_0 c_0 d_0) (a_1 b_1 c_1 d_1) (a_2 b_2 c_2 d_2) ...), and a number of
-  frames, returns another sequence of de jong paramters with num-frames frames
-  interposed
-  ((a_0 b_0 c_0 d_0) {num-frames more frames} (a_1 b_1 c_1 d_1)
-  {num-frame more frames} (a_2 b_2 c_2 d_2) ...)"
+  ((a_0 b_0 c_0 d_0) (a_1 b_1 c_1 d_1)
+  (a_2 b_2 c_2 d_2) ... (a_n b_n c_n d_n)), and a
+  number of frames, returns another sequence of de jong
+  paramters with num-frames frames interposed
+  (v_0 {num-frames more frames}
+   v_1 {num-frames more frames}
+   v_2 {num-frames more frames}
+   ...
+   v_n {num-frames more frames})"
   [num-frames xs]
-  (let [pairs (partition 2 1 xs)]
+  (let [pairs (partition 2 1 (conj xs (first xs)))]
     (mapcat (fn [[a b]] (lerp-vectors num-frames a b)) pairs)))
