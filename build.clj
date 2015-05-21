bb51b7f96fb6ab52dbad7dbdc097f1d601794bec
Chris LaRose <chris.larose@mycase.com>
2015-05-20 20:01:10 -0700

    Remove now-unused build files

--- build.clj
+++
@@ -1,5 +0,0 @@
-(require 'cljs.closure)
-
-(cljs.closure/build "src"
-    {:main 'de-jong.core
-     :output-to "out/main.js"})
--- watch.clj
+++
@@ -1,5 +0,0 @@
-(require 'cljs.closure)
-
-(cljs.closure/watch "src"
-  {:main 'de-jong.core
-   :output-to "out/main.js"})
