3cdcfc34439d7ea99ec2fc3eaccd83a199a7eaaa
Chris LaRose <chris.larose@mycase.com>
2015-05-20 21:37:26 -0700

    cjl - Begin experimenting with canvas rendering

--- src/de_jong/components/point-cloud.cljs
+++ src/de_jong/components/point-cloud.cljs
@@ -2,8 +2,7 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om]
             [om.dom :as dom]
-            [cljs.core.async :refer [<! chan put!]]
-            [cljsjs.three]))
+            [cljs.core.async :refer [<! chan put!]]))
 
 (defn handle-resize [owner _]
   (let [{:keys [renderer camera]} (om/get-state owner)
@@ -13,41 +12,41 @@
     (.updateProjectionMatrix camera)
     (.setSize renderer w h)))
 
+(defn setup-canvas! [canvas]
+  (let [context (.getContext canvas "2d")]
+    (set! (.-fillStyle context) "rgba(0, 192, 0, 0.5)")
+    (.scale context 200 200)
+    (.translate context 2 2)))
+
 (defn draw! [owner draw-chan]
   (go (while true
     (let [points (<! draw-chan)]
       (if-not (nil? points)
-        (let [vertex-attr (js/THREE.BufferAttribute. points 3)
-              {:keys [geometry renderer scene camera cloud]} (om/get-state owner)]
-          (.addAttribute geometry "position" vertex-attr)
-          (.render renderer scene camera)))))))
+        (let [canvas (om/get-node owner "canvas")
+              context (.getContext canvas "2d")
+              { :keys [width height] } (om/get-state owner)
+              image-data (.createImageData context width height)]
+          (.clearRect context -2 -2 4 4)
+          (doseq [[x y _] (partition 3 (array-seq points))]
+            (let [fx (js/Math.floor (* 200 (+ x 2)))
+                  fy (js/Math.floor (* 200 (+ y 2)))]
+              (aset (.-data image-data) (* (+ (* fy width) fx) 4) 255)
+              (aset (.-data image-data) (+ (* (+ (* fy width) fx) 4) 3) 255)))
+          (.putImageData context image-data 0 0)))))))
 
 (defn point-cloud [draw-chan owner]
   (reify
     om/IInitState
     (init-state [_]
       (let [width    (.-innerWidth js/window)
-            height   (.-innerHeight js/window)
-            geometry (js/THREE.BufferGeometry.)
-            scene    (js/THREE.Scene.)
-            camera   (js/THREE.PerspectiveCamera. 45 (/ width height) 0.1 1000)
-            material (js/THREE.PointCloudMaterial. #js { :size 0.02 :color 0x00cc00 })
-            cloud    (js/THREE.PointCloud. geometry material)]
-        (.add scene cloud)
-        (set! (.-z (.-position camera)) 8)
-        { :geometry geometry
-          :scene scene
-          :camera camera
-          :cloud cloud }))
+            height   (.-innerHeight js/window)]
+        { :width width
+          :height height }))
     om/IDidMount
     (did-mount [_]
-      (let [renderer (js/THREE.WebGLRenderer. #js { :canvas (om/get-node owner "canvas")
-                                                    :alpha true })]
-        (.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window))
-        (.addEventListener js/window "resize" (partial handle-resize owner))
-        (draw! owner draw-chan)
-        (om/update-state! owner (fn [prev] (merge prev { :renderer renderer })))))
-    om/IRender
-    (render [_]
+        (setup-canvas! (om/get-node owner "canvas"))
+        (draw! owner draw-chan))
+    om/IRenderState
+    (render-state [_ { :keys [width height] }]
       (dom/div #js {:id "point-cloud"}
-        (dom/canvas #js { :ref "canvas" })))))
+        (dom/canvas #js { :ref "canvas" :width width :height height})))))
