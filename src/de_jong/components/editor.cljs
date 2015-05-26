d841808817b8a83fe987bcca3b320829687f697e
Chris LaRose <cjlarose@gmail.com>
2015-05-25 21:43:55 -0700

    cjl - Make previews render fewer, larger points

--- src/de_jong/components/editor.cljs
+++ src/de_jong/components/editor.cljs
@@ -3,13 +3,13 @@
             [om.dom :as dom]
             [cljs.core.async :refer [chan put!]]
             [de-jong.components.point-cloud :refer [point-cloud]]
-            [de-jong.points-calculator :refer [points-to-draw
-                                               de-jong-ifs
+            [de-jong.points-calculator :refer [de-jong-ifs
                                                random-vertex-array
                                                vertices-apply]]))
 
 (defn draw-points! [draw-chan params]
-  (let [random-array (random-vertex-array points-to-draw -2.0 2.0)
+  (let [points-to-draw (js/Math.pow 2 9)
+        random-array (random-vertex-array points-to-draw -2.0 2.0)
         my-ifs       (apply de-jong-ifs params)
         points-array (vertices-apply my-ifs random-array)]
     (put! draw-chan points-array)))
@@ -30,7 +30,7 @@
     (render-state [this { :keys [draw-chan] }]
       (dom/li #js {:className (str "preview" (if selected " selected"))}
         (dom/a #js {:href "#" :onClick (fn [e] (.preventDefault e) (onSelect))}
-          (om/build point-cloud { :draw-chan draw-chan :point-size 0.05 :width 150 :height 100 }))))))
+          (om/build point-cloud { :draw-chan draw-chan :point-size 0.25 :width 150 :height 100 }))))))
 
 (defn preview-params [selection idx params]
   { :onSelect (fn [] (om/transact! selection #(assoc % :idx idx)))
