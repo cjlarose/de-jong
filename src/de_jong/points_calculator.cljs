306db0af175de6f457ba3ad67a90229c8eb4ba80
Chris LaRose <cjlarose@gmail.com>
2015-05-28 00:56:10 -0700

    cjl - Remove now-unused functions

--- src/de_jong/points_calculator.cljs
+++ src/de_jong/points_calculator.cljs
@@ -5,13 +5,6 @@
 (defn in-range [x]
   (and (>= x (- js/Math.PI)) (<= x js/Math.PI)))
 
-(defn de-jong-ifs [a b c d]
-  {:pre [(every? in-range [a b c d])]}
-  (fn [x y _]
-    [(- (js/Math.sin (* a y)) (js/Math.cos (* b x)))
-     (- (js/Math.sin (* c x)) (js/Math.cos (* d y)))
-     0]))
-
 (defn- vertex-array [length]
   (js/Float32Array. (* 3 length)))
 
@@ -31,18 +24,3 @@
 (defn random-vertex-array [length minimum maximum]
   (let [arr (vertex-array length)]
     (write-random-values! minimum maximum arr)))
-
-(defn vertices-apply [f vertices]
-  (let [length  (.-length vertices)
-        new-arr (vertex-array (/ length 3))]
-    (loop [i 0]
-      (if (< i length)
-        (let [x (aget vertices i)
-              y (aget vertices (+ i 1))
-              z (aget vertices (+ i 2))
-              [x2 y2 z2] (f x y z)]
-          (aset new-arr i x2)
-          (aset new-arr (+ i 1) y2)
-          (aset new-arr (+ i 2) z2)
-          (recur (+ i 3) ))))
-    new-arr))
