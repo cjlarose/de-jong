8bc07b44ea6979c65d01ff7ed0f0832e039cc37e
Chris LaRose <cjlarose@gmail.com>
2015-05-27 23:00:57 -0700

    Reimplemented De Jong calculation with custom vertex shader
      * Same basic thing with shaders
      * Uniform working
      * Basic animation using de jong params
      * Remove old code
      * Move point setting to point cloud init
      * More iterations
      * Update editor previews to send in correct params to point clouds
      * Remove now-unused functions
      * Remove intermediate floats
      * Point size uniform
      * Configurable number of points to render
      * Fix de jong calculation

--- src/de_jong/components/app.cljs
+++ src/de_jong/components/app.cljs
@@ -7,29 +7,19 @@
             [de-jong.components.point-cloud :refer [full-screen-point-cloud]]
             [de-jong.components.editor :refer [editor]]
             [de-jong.components.params-picker :refer [params-picker]]
-            [de-jong.points-calculator :refer [points-to-draw
-                                               de-jong-ifs
-                                               random-vertex-array
-                                               vertices-apply]]
             [de-jong.transition :refer [transition-params]]))
  
-(defn calculator-channel [params-chan initial-seq]
+(defn calculator-channel [scene-chan initial-seq]
   (let [throttler    (animation-frame)
-        draw-chan    (chan)
-        random-array (random-vertex-array points-to-draw -2.0 2.0)
-        points-array (atom random-array)
+        params-chan  (chan)
         params-seq   (atom initial-seq)]
     (go (while true
-      (>! draw-chan @points-array)
-      (let [[v port] (alts! [params-chan throttler])]
-        (if (= port params-chan)
-          (do
-            (reset! params-seq v)
-            (reset! points-array random-array)))
-        (let [ifs (apply de-jong-ifs (first @params-seq))]
-          (swap! points-array (partial vertices-apply ifs))
+      (>! params-chan (first @params-seq))
+      (let [[v port] (alts! [scene-chan throttler])]
+        (if (= port scene-chan)
+          (reset! params-seq v)
           (swap! params-seq rest)))))
-    draw-chan))
+    params-chan))
 
 (def make-transition (comp cycle (partial transition-params 120)))
 
@@ -37,20 +27,20 @@
   (reify
     om/IInitState
     (init-state [_]
-      (let [params-chan (chan)
+      (let [scene-chan (chan)
             initial-seq (make-transition ifs-params)]
-        {:params-chan params-chan
-         :draw-chan   (calculator-channel params-chan initial-seq)}))
+        {:scene-chan scene-chan
+         :draw-chan   (calculator-channel scene-chan initial-seq)}))
     om/IWillReceiveProps
     (will-receive-props [this {:keys [ifs-params] :as next-props}]
       (let [old-ifs-params (om/get-props owner :ifs-params)]
         (if-not (= old-ifs-params ifs-params)
-          (let [params-chan (om/get-state owner :params-chan)]
-            (put! params-chan (make-transition ifs-params))))))
+          (let [scene-chan (om/get-state owner :scene-chan)]
+            (put! scene-chan (make-transition ifs-params))))))
     om/IRenderState
     (render-state [this {:keys [draw-chan]}]
       (dom/div nil
         (om/build editor data)
         (dom/div #js {:className "params-picker-container"}
           (om/build params-picker (nth ifs-params (:idx selection))))
-        (om/build full-screen-point-cloud draw-chan)))))
+        (om/build full-screen-point-cloud [draw-chan (js/Math.pow 2 13)])))))
