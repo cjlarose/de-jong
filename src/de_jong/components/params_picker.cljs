535c2fb4316c3cdeeffcb7df7ca90f41be724f2d
Chris LaRose <cjlarose@gmail.com>
2015-05-30 15:29:57 -0700

    cjl - Replace circular sliders with boring sliders

--- src/de_jong/components/params_picker.cljs
+++ src/de_jong/components/params_picker.cljs
@@ -1,17 +1,26 @@
 (ns de-jong.components.params-picker
   (:require [om.core :as om]
-            [om.dom :as dom]
-            [de-jong.components.circular-slider :refer [circular-slider]]))
+            [om.dom :as dom]))
+
+(defn slider [{ :keys [value on-change] } owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/input #js { :type "range"
+                       :min (- js/Math.PI)
+                       :max js/Math.PI
+                       :step 0.001
+                       :onChange (fn [e] (on-change (js/parseFloat (.-value (.-target e)))))
+                       :value value }))))
 
 (defn param-picker [{:keys [label value on-change]} owner]
   (reify
     om/IRender
     (render [this]
       (dom/li #js { :className "param-picker" }
-        (dom/div #js { :className "param-display" }
-          (dom/span #js { :className "param-label" } label)
-          (dom/span #js { :className "param-value" } (.toFixed value 3)))
-        (om/build circular-slider {:value value :on-change on-change})))))
+        (dom/span #js { :className "param-label" } label)
+        (om/build slider { :value value :on-change on-change })
+        (dom/span #js { :className "param-value" } (.toFixed value 3))))))
 
 (defn params-picker [params owner]
   (reify
@@ -24,7 +33,7 @@
                            { :label label
                              :value v
                              :on-change (partial handle-change i) })]
-        (dom/section #js { :className "params-picker noselect" }
+        (dom/section #js { :className "params-picker" }
           (apply dom/ul nil
             (om/build-all param-picker (map picker-props (range 4) param-labels params))))))))
 
