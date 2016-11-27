b627e46e3e3c42ca3fa3cf786218086211a1e6d7
Chris LaRose <cjlarose@gmail.com>
2016-11-27 08:53:47 -0800

    Frame-rate-independent rendering

--- src/de_jong/components/app.cljs
+++ src/de_jong/components/app.cljs
@@ -4,28 +4,24 @@
             [de-jong.components.point-cloud :refer [full-screen-point-cloud]]
             [de-jong.components.top-bar :refer [top-bar]]
             [de-jong.components.editor :refer [editor]]
-            [de-jong.transition :refer [transition-params]]))
+            [de-jong.transition :refer [transition-params-fn]]))
 
 (defn advance-frame! [owner]
   (let [{ :keys [frames frame-number] } (om/get-state owner)]
     (om/set-state! owner :frame-number (mod (inc frame-number) (count frames)))))
 
-(def make-transition (comp vec (partial transition-params 120)))
-
 (defn app [{:keys [ifs-params] :as data} owner]
   (reify
     om/IInitState
     (init-state [_]
-      { :frames (make-transition ifs-params)
+      { :param-gen (transition-params-fn ifs-params)
         :frame-number 0 })
     om/IWillReceiveProps
     (will-receive-props [this {:keys [ifs-params] :as next-props}]
       (let [old-ifs-params (om/get-props owner :ifs-params)]
         (if-not (= old-ifs-params ifs-params)
-          (let [new-frames    (make-transition ifs-params)
-                current-frame (om/get-state owner :frame-number)]
-            (om/set-state! owner { :frames new-frames
-                                   :frame-number (mod current-frame (count new-frames)) })))))
+          (let [new-param-gen (transition-params-fn ifs-params)]
+            (om/set-state! owner { :param-gen new-param-gen })))))
     om/IDidMount
     (did-mount [_]
       (advance-frame! owner))
@@ -33,9 +29,10 @@
      (did-update [_ _ _]
        (advance-frame! owner))
     om/IRenderState
-    (render-state [this {:keys [frames frame-number]}]
-      (dom/div nil
-        (dom/header nil
-          (om/build top-bar data)
-          (om/build editor data))
-        (om/build full-screen-point-cloud [(nth frames frame-number) (js/Math.pow 2 18)])))))
+    (render-state [this {:keys [param-gen]}]
+      (let [t (/ (.now (.-performance js/window)) (* 2000 (count ifs-params)))]
+        (dom/div nil
+          (dom/header nil
+            (om/build top-bar data)
+            (om/build editor data))
+          (om/build full-screen-point-cloud [(param-gen t) (js/Math.pow 2 18)]))))))
--- src/de_jong/transition.cljs
+++ src/de_jong/transition.cljs
@@ -4,22 +4,21 @@
   {:pre [(and (>= t 0) (<= t 1))]}
   (+ initial (* t (- final initial))))
 
-(defn lerp-vectors [num-frames initial final]
-  (let [step     (/ 1 num-frames)
-        vec-lerp (fn [t i f] (map (partial lerp t) i f))]
-    (map #(vec-lerp % initial final) (range 0 1 step))))
+(defn interpolate-param-pair [xs ys]
+  (fn [t]
+    (map (fn [x y] (lerp t x y)) xs ys)))
 
-(defn transition-params
+(defn transition-params-fn
   "For a sequence of de jong parameters
   ((a_0 b_0 c_0 d_0) (a_1 b_1 c_1 d_1)
-  (a_2 b_2 c_2 d_2) ... (a_n b_n c_n d_n)), and a
-  number of frames, returns another sequence of de jong
-  paramters with num-frames frames interposed
-  (v_0 {num-frames more frames}
-   v_1 {num-frames more frames}
-   v_2 {num-frames more frames}
-   ...
-   v_n {num-frames more frames})"
-  [num-frames xs]
-  (let [pairs (partition 2 1 (conj xs (first xs)))]
-    (mapcat (fn [[a b]] (lerp-vectors num-frames a b)) pairs)))
+  (a_2 b_2 c_2 d_2) ... (a_n b_n c_n d_n)), returns a function of
+  t, s.t. for inputs from 0 to 1, t returns a set of de jong
+  params interpolated between the inputs"
+  [xs]
+  (let [pairs (partition 2 1 (conj xs (first xs)))
+        fns   (map (fn [[a b]] (interpolate-param-pair a b)) pairs)]
+    (fn [t]
+      (let [fi (mod (Math/floor (* t (count fns))) (count fns))
+            f  (nth fns fi)
+            t2 (* (mod t (/ (count fns))) (count fns))]
+        (f t2)))))
